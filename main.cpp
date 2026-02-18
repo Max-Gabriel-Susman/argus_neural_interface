570f6af883e1b3e7ce8b623166c742017ec9b662
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-02-17 17:35:15 -0700

    included an entrypoint for the interface logic

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 
 int main() {
-    std::cout << "Initializing Argus Neural Interface..." << std::endl;
-    std::cout << "Argus Neural Interface Online." << std::endl;
+  std::cout << "Initializing Argus Neural Interface..." << std::endl;
+  std::cout << "Argus Neural Interface Online." << std::endl;
+  std::cout << "Listening for neural data..." << std::endl;    
 }
